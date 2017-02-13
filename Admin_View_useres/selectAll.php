74154a9e028b5bbbd97c42ab02e2bdc4e0bdd986
mariamEraQi <mariam.eleraki@gmail.com>
2017-02-14 00:48:03 +0200

    selectAll
    query page

---
+++ Admin_View_useres/selectAll.php
@@ -0,0 +1,45 @@
+<?php
+
+
+$dbhost= 'localhost';
+$dbuser= 'root';
+$dbpass='iti';
+$dbname='ecommerce';
+// open connection
+$mysqli = new mysqli($dbhost, $dbuser,$dbpass);
+$mysqli->select_db($dbname);
+
+$name=$_POST['username'];
+if($name == "choose user"){
+    
+    http_response_code(400);
+}
+
+$query="select * from users where username=? ";
+$stmt = $mysqli->prepare($query);
+
+if(!$stmt){
+    echo "preparation failed ".$mysqli->errno." : ".$mysqli->error."<br>";
+}
+
+
+
+$stmt->bind_param('s',$name );
+$stmt->execute();   
+$result = $stmt->get_result();
+$row = $result->fetch_array();
+if($row){
+  
+    $result = [$row['username'],$row['email'],
+               $row['address'],$row['job'],
+               $row['birthDate'],$row['creditLimit']];
+   
+}
+else
+    return false;
+
+echo json_encode($result);
+
+
+    
+?>
